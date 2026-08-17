69cf499153f3a45315f607c424e187d5dfa3dc7d
Pratik <sonawanepratik578@gmail.com>
2026-08-17 15:18:59 +0530

    Add calculator project

---
+++ 03_Operators/calculator.py
@@ -0,0 +1,22 @@
+#==============Using opertors Build Calsculator===============#
+
+num1 = int(input("Enter your first number:"))
+num2 = int(input("Enter your second number:"))
+
+add = (num1 + num2)
+sub = (num1 - num2)
+mul = (num1 * num2)
+div = (num1 / num2)
+rem = (num1 % num2)
+quo = (num1 // num2)
+squ = (num1 ** 2),(num2 ** 2)
+cube = (num1 ** 3),(num2 ** 3)
+
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
+print("Remainder:",rem)
+print("Quotient:",quo)
+print("Square:",squ)
+print("Cube:",cube)
